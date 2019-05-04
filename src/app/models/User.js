4e3ae3ca1a09793192c88a9b120167889ec0f2a3
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2019-05-04 11:02:14 -0300

    Change the Login, Send message call

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -2,7 +2,6 @@
 /* eslint-disable consistent-return */
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -26,19 +25,5 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
-
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-
-  this.password = await bcrypt.hash(this.password, 8);
-});
-
-UserSchema.methods = {
-  compareHash(password) {
-    return bcrypt.compare(password, this.password);
-  },
-};
 
 module.exports = mongoose.model('User', UserSchema);
